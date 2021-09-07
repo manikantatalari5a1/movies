97914f32c989843f0c8cb253c218acbddccf7617
manikantatalari5a1 <89739904+manikantatalari5a1@users.noreply.github.com>
2021-09-07 15:01:13 +0530

    Add files via upload

---
+++ database_conn.php
@@ -0,0 +1,17 @@
+<?php
+$servername='localhost';
+$username='root';
+$password='';
+$database='mulesoft';
+$conn=mysqli_connect($servername,$username,$password,$database);
+if($conn)
+{
+    
+}
+else
+{
+    echo "not connectd";
+}
+?>
+
+
